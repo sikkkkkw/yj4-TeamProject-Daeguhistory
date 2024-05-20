f8fe5a76fb551cda91fccc20830c2d5fbcb8feda
sikkkkkw <gksmsla92@naver.com>
2024-05-20 17:51:16 +0900

    feat: 유저정보 api추가

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { loginUser, registerUser } from '../controllers/userController.js';
+import { loginUser, profileUser, registerUser } from '../controllers/userController.js';
+import { authenticationRequired } from '../middleware/auth.js';
 
 const userRouter = express.Router();
 
 userRouter.post('/login', loginUser);
 
 userRouter.post('/register', registerUser);
+
+userRouter.post('/profile',authenticationRequired, profileUser);
 
 export default userRouter;
