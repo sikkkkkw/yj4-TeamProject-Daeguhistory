a29008536b2a287a6d7d598cf05fab32d2c3a24c
sikkkkkw <gksmsla92@naver.com>
2024-05-21 15:22:21 +0900

    style: http상태코드 변경

--- src/controllers/tokenController.js
+++ src/controllers/tokenController.js
@@ -17,7 +17,7 @@ export const refreshToken = async (req, res) => {
         const queryCheckRefreshToken = 'SELECT * FROM refresh_tokens WHERE token = ?';
         const resultCheckRefreshToken = await db.execute(queryCheckRefreshToken, [receivedToken]).then((result) => result[0][0]);
         if (!resultCheckRefreshToken) {
-            return res.status(403).json({ status: 'fail', message: '유효하지 않은 리프레시 토큰입니다.' });
+            return res.status(401).json({ status: 'fail', message: '유효하지 않은 리프레시 토큰입니다.' });
         }
 
         // 새로운 액세스 토큰 생성
@@ -37,9 +37,9 @@ export const refreshToken = async (req, res) => {
         res.status(200).json({ status: 'success', accessToken: newAccessToken, refreshToken: newRefreshToken });
     } catch (err) {
         if (err.name === 'TokenExpiredError') {
-            return res.status(403).json({ status: 'fail', message: '리프레시 토큰이 만료되었습니다.' });
+            return res.status(401).json({ status: 'fail', message: '리프레시 토큰이 만료되었습니다.' });
         }
         console.log(err);
-        res.status(403).json({ status: 'fail', message: '서버 에러' });
+        res.status(500).json({ status: 'fail', message: '서버 에러' });
     }
 };
